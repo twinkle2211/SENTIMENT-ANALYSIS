302dfb2b698ba9daf71725c154f2ca515598ac2d
twinkle2211 <91751462+twinkle2211@users.noreply.github.com>
2022-07-19 10:30:11 -0700

    Add files via upload

---
+++ twitter_analysis.py
@@ -0,0 +1,69 @@
+import string
+from collections import Counter
+
+import matplotlib.pyplot as plt
+
+
+def get_tweets():
+    import twint
+    import schedule
+    import time
+    c=twint.Config()
+    c.Search="Donal-Trump"
+    c.Since="2020-11-4"
+    c.Limit=1000
+    c.Store_csv=True
+    twint.run.Search(c)
+    # Creating list of chosen tweet data
+    text_tweets = [[tweet.text] for tweet in c]
+    return text_tweets
+
+
+# reading text file
+text = ""
+text_tweets = get_tweets()
+length = len(text_tweets)
+
+for i in range(0, length):
+    text = text_tweets[i][0] + " " + text
+
+# converting to lowercase
+lower_case = text.lower()
+
+# Removing punctuations
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+
+# splitting text into words
+tokenized_words = cleaned_text.split()
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+# Removing stop words from the tokenized words list
+final_words = [word for word in tokenized_words if word not in stop_words]
+
+# Get emotions text
+emotion_list = []
+with open('emotions.txt', 'r') as file:
+    for line in file:
+        clear_line = line.replace('\n', '').replace(',', '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+        if word in final_words:
+            emotion_list.append(emotion)
+
+w = Counter(emotion_list)
+print(w)
+
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
